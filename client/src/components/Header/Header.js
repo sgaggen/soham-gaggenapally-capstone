26a13dcea0db37502a9d2c9b4688b4112f38dd07
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-02-01 00:04:56 -0500

    Add conditional display of login and sign up buttons

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,8 +6,10 @@ function Header() {
             HearHere
             <nav>
                 <Link to='/home'>Home</Link>
-                <Link to='/login'>Log In</Link>
-                <Link to='/signup'>Sign Up</Link>
+                {/* <Link to='/login'>Log In</Link>
+                <Link to='/signup'>Sign Up</Link> */}
+                {window.sessionStorage.getItem("userId") ? "" : <Link to='/login'>Log In</Link>}
+                {window.sessionStorage.getItem("userId") ? "" : <Link to='/signup'>Sign Up</Link>}
                 {window.sessionStorage.getItem("userId") ? <Link to='/account'>Account</Link> : ""}
             </nav>
         </header>
