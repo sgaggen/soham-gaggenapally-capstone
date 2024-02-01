747e875acef73616ab87591c566a541b354f8114
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-02-01 00:20:10 -0500

    Add log out functionality

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+    const navigate = useNavigate();
+
+    function handleLogout () {
+        sessionStorage.removeItem('userId');
+        alert("logged out!")
+        navigate('/home')
+    }
+
     return (
         <header>
             HearHere
@@ -11,6 +20,7 @@ function Header() {
                 {window.sessionStorage.getItem("userId") ? "" : <Link to='/login'>Log In</Link>}
                 {window.sessionStorage.getItem("userId") ? "" : <Link to='/signup'>Sign Up</Link>}
                 {window.sessionStorage.getItem("userId") ? <Link to='/account'>Account</Link> : ""}
+                {window.sessionStorage.getItem("userId") ? <Link to='' onClick={handleLogout}>Log Out</Link> : ""}
             </nav>
         </header>
     )
