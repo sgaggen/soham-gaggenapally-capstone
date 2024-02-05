0914a53098cd7998c3dcb31f9d778678c84fbf00
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-02-04 23:28:29 -0500

    Add create playlist workflow

--- client/src/components/Playlists/Playlists.js
+++ client/src/components/Playlists/Playlists.js
@@ -50,6 +50,26 @@ function Playlists() {
         }
     }
 
+    async function handleAddPlaylist (event) {
+        event.preventDefault();
+        const name = event.target.playlistsInput.value;
+        const id = window.sessionStorage.getItem("userId");
+
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/playlist`, {
+                user_id: id,
+                playlist_name: name,
+                playlist_group_id: name.replace(/ /g, "_") + "_" + id
+            });
+            console.log('client end of creating a playlist:', response.data)
+            event.target.reset();
+            setShowAddPlaylist(false);
+            getPlaylists();
+        } catch (error) {
+            console.log('client error creating a playlist:', error)
+        }
+    }
+
     useEffect(() => { getPlaylists() }, []);
 
 
@@ -73,9 +93,18 @@ function Playlists() {
                         <Link to={`/playlist/${playlist.playlist_group_id}`} className='song'>{playlist.playlist_name}</Link>
                     </div>
                 )}
-                {/* <button className="song button--add">+</button> */}
+
                 {showAddPlaylist ?
-                    <div className='song' onClick={() => setShowAddPlaylist(false)}>div</div>
+                    <form className='playlists__form' onSubmit={handleAddPlaylist} onBlur={() => setShowAddPlaylist(false)}>
+                        <label htmlFor="playlists-input" className='invisible'>Search</label>
+                        <input
+                            type="text"
+                            name="playlistsInput"
+                            id="playlists-input"
+                            className='playlists-input'
+                            placeholder="enter a name"
+                        />
+                    </form>
                     :
                     <button className="song button--add" onClick={() => setShowAddPlaylist(true)}>+</button>
                 }
